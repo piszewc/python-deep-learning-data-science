22ca5e3f6b15f5eed6c9ac53f2fb9a1f0d6c9613
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-11-01 21:12:34 +0100

    Week 2

---
+++ Python/Python - MIT 6.0/Week 2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 12:06:13 2018
+
+@author: piotr
+"""
+
+iteration = 0
+while iteration < 5:
+    count = 0
+    for letter in "hello, world":
+        count += 1
+        if iteration % 2 == 0:
+            break
+    print("Iteration " + str(iteration) + "; count is: " + str(count))
+    iteration += 1 
