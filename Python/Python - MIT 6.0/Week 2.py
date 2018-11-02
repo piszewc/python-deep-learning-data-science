856839e840ddfbee3e54faf4d2ad141d8aee6d81
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-11-02 21:02:52 +0100

    Week 2 - update

--- Python/Python - MIT 6.0/Week 2.py
+++ Python/Python - MIT 6.0/Week 2.py
@@ -14,3 +14,32 @@ while iteration < 5:
             break
     print("Iteration " + str(iteration) + "; count is: " + str(count))
     iteration += 1 
+    
+    
+    
+#guess my number
+    
+secret_number = 36
+
+numGuesses = 0
+epsilon = 1
+low = 0
+high = 100
+ans = int((high+low)/2)
+
+print("Please think of a number between 0 and 100!")
+print("Is your secret number: " +str(ans))
+text = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+while text != "c":
+    
+    if text == "h":
+        high = ans
+    elif text == "l":
+        low = ans
+    ans = int((high + low)/2)
+    
+    print("Is your secret number: " +str(ans))
+    text = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
+    numGuesses += 1
+
+print("Game over. Your secret number was: " +str(ans))
