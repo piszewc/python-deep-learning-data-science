fe98aa3b6b69fc52fef01f0ca210d15e35f01fb4
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-11-07 20:42:16 +0100

    Week 2

--- Python/Python - MIT 6.0/Week 2.py
+++ Python/Python - MIT 6.0/Week 2.py
@@ -53,3 +53,32 @@ print(x)
 
 #Functions
 
+def printname(firstName,lastName, reverse = False):
+    if reverse:
+        print(lastName+", "+firstName)
+    else:
+        print(firstName, lastName)
+        
+printname("Piotr","Szewc", True)
+
+def multi(a,b):
+    if b==1:
+        return a
+    else:
+        return a+multi(a,b-1)
+multi(2,8)
+
+#Tower of Hanoi
+
+def printMove(fr,to):
+    print("move from "+ str(fr)+ " to "+str(to))
+
+def Towers(n, fr, to, spare):
+    if n == 1:
+        printMove(fr,to)
+    else:
+        Towers(n-1, fr, spare, to)
+        Towers(1, fr, to, spare)
+        Towers(n-1, spare, to, fr)
+        
+print(Towers(4,"P1","p2","p3"))
