499f27ec7f216712f479f6f9abe00c8a7056b1f3
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2019-04-17 22:36:36 +0200

    update classification

--- tensorflow/spam_classification.py
+++ tensorflow/spam_classification.py
@@ -108,3 +108,128 @@ def email_to_text(email):
         return html_to_plain_text(html)
     
 print(email_to_text(sample_html_spam)[:100], "...")
+
+try:
+    import nltk
+
+    stemmer = nltk.PorterStemmer()
+    for word in ("Computations", "Computation", "Computing", "Computed", "Compute", "Compulsive"):
+        print(word, "=>", stemmer.stem(word))
+except ImportError:
+    print("Error: stemming requires the NLTK module.")
+    stemmer = None
+    
+    
+try:
+    import urlextract # may require an Internet connection to download root domain names
+    
+    url_extractor = urlextract.URLExtract()
+    print(url_extractor.find_urls("Will it detect github.com and https://youtu.be/7Pq-S557XQU?t=3m32s"))
+except ImportError:
+    print("Error: replacing URLs requires the urlextract module.")
+    url_extractor = None
+    
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class EmailToWordCounterTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, strip_headers=True, lower_case=True, remove_punctuation=True,
+                 replace_urls=True, replace_numbers=True, stemming=True):
+        self.strip_headers = strip_headers
+        self.lower_case = lower_case
+        self.remove_punctuation = remove_punctuation
+        self.replace_urls = replace_urls
+        self.replace_numbers = replace_numbers
+        self.stemming = stemming
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X, y=None):
+        X_transformed = []
+        for email in X:
+            text = email_to_text(email) or ""
+            if self.lower_case:
+                text = text.lower()
+            if self.replace_urls and url_extractor is not None:
+                urls = list(set(url_extractor.find_urls(text)))
+                urls.sort(key=lambda url: len(url), reverse=True)
+                for url in urls:
+                    text = text.replace(url, " URL ")
+            if self.replace_numbers:
+                text = re.sub(r'\d+(?:\.\d*(?:[eE]\d+))?', 'NUMBER', text)
+            if self.remove_punctuation:
+                text = re.sub(r'\W+', ' ', text, flags=re.M)
+            word_counts = Counter(text.split())
+            if self.stemming and stemmer is not None:
+                stemmed_word_counts = Counter()
+                for word, count in word_counts.items():
+                    stemmed_word = stemmer.stem(word)
+                    stemmed_word_counts[stemmed_word] += count
+                word_counts = stemmed_word_counts
+            X_transformed.append(word_counts)
+        return np.array(X_transformed)
+    
+X_few = X_train[:3]
+X_few_wordcounts = EmailToWordCounterTransformer().fit_transform(X_few)
+X_few_wordcounts
+
+from scipy.sparse import csr_matrix
+
+class WordCounterToVectorTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, vocabulary_size=1000):
+        self.vocabulary_size = vocabulary_size
+    def fit(self, X, y=None):
+        total_count = Counter()
+        for word_count in X:
+            for word, count in word_count.items():
+                total_count[word] += min(count, 10)
+        most_common = total_count.most_common()[:self.vocabulary_size]
+        self.most_common_ = most_common
+        self.vocabulary_ = {word: index + 1 for index, (word, count) in enumerate(most_common)}
+        return self
+    def transform(self, X, y=None):
+        rows = []
+        cols = []
+        data = []
+        for row, word_count in enumerate(X):
+            for word, count in word_count.items():
+                rows.append(row)
+                cols.append(self.vocabulary_.get(word, 0))
+                data.append(count)
+        return csr_matrix((data, (rows, cols)), shape=(len(X), self.vocabulary_size + 1))
+    
+    
+vocab_transformer = WordCounterToVectorTransformer(vocabulary_size=10)
+X_few_vectors = vocab_transformer.fit_transform(X_few_wordcounts)
+X_few_vectors
+
+X_few_vectors.toarray()
+
+vocab_transformer.vocabulary_
+
+from sklearn.pipeline import Pipeline
+
+preprocess_pipeline = Pipeline([
+    ("email_to_wordcount", EmailToWordCounterTransformer()),
+    ("wordcount_to_vector", WordCounterToVectorTransformer()),
+])
+
+X_train_transformed = preprocess_pipeline.fit_transform(X_train)
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+
+log_clf = LogisticRegression(solver="liblinear", random_state=42)
+score = cross_val_score(log_clf, X_train_transformed, y_train, cv=3, verbose=3)
+score.mean()
+
+from sklearn.metrics import precision_score, recall_score
+
+X_test_transformed = preprocess_pipeline.transform(X_test)
+
+log_clf = LogisticRegression(solver="liblinear", random_state=42)
+log_clf.fit(X_train_transformed, y_train)
+
+y_pred = log_clf.predict(X_test_transformed)
+
+print("Precision: {:.2f}%".format(100 * precision_score(y_test, y_pred)))
+print("Recall: {:.2f}%".format(100 * recall_score(y_test, y_pred)))
