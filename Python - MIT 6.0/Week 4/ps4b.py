f6195fe09c9be0dfe83e5a488a5aa8e1ad3e1d08
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2019-07-13 10:33:55 +0200

    Update ps4b.py

--- Python - MIT 6.0/Week 4/ps4b.py
+++ Python - MIT 6.0/Week 4/ps4b.py
@@ -127,17 +127,19 @@ def playGame(wordList):
     
     
     while True:
-        
-        user_input = input("Please input")
-        
-        if user_input == "n":
+        user_input = input('Enter n to deal a new hand, r to replay the last hand, or e to end game: ')
+        if user_input == 'e':
             break
-        elif user_input == "r":
-            break
-        elif user_input == "e":
-            break
+        elif user_input == 'n':
+            hand = dealHand(HAND_SIZE)
+            playHand(hand, wordList, HAND_SIZE)
+        elif user_input == 'r':
+            try:
+                playHand(hand, wordList, HAND_SIZE)
+            except:
+              print('You have not played a hand yet. Please play a new hand first!')                
         else:
-            print("Please enter correct letter")
+            print('Invalid command.')
 
         
 #
