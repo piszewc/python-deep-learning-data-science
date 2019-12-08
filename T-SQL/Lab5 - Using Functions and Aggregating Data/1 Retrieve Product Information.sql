bf31f0273180e5a773ba04a787c98ae574445375
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2019-12-08 19:41:16 +0100

    SQL

--- T-SQL/Lab5 - Using Functions and Aggregating Data/1 Retrieve Product Information.sql
+++ T-SQL/Lab5 - Using Functions and Aggregating Data/1 Retrieve Product Information.sql
@@ -1,10 +1,17 @@
--- Retrieve the year and month in which products were first sold
-SELECT YEAR(SellStartDate) SellStartYear, DATENAME(mm,SellStartDate) SellStartMonth,
-	   ProductID, Name
-FROM SalesLT.Product
-ORDER BY SellStartYear;
+-- Retrieve the name and approximate weight of each product
+SELECT p.ProductID as ProductID, UPPER(p.Name) AS Name, ROUND(Weight,0) as ApproxWeight
+FROM SalesLT.Product AS p
 
+-- Retrieve the year and month in which products were first sold
+SELECT p.ProductID as ProductID, UPPER(p.Name) AS Name, ROUND(Weight,0) as ApproxWeight, YEAR(p.SellStartDate) as SellStartYear , DATENAME(mm,p.SellStartDate) as SellStartMonth
+FROM SalesLT.Product AS p
 
 -- Extract product types from product numbers
-SELECT Name, ProductNumber, LEFT(ProductNumber, 2) AS ProductType
-FROM SalesLT.Product;
+SELECT p.ProductID as ProductID, UPPER(p.Name) AS Name, ROUND(Weight,0) as ApproxWeight, YEAR(p.SellStartDate) as SellStartYear , DATENAME(mm,p.SellStartDate) as SellStartMonth, LEFT(p.ProductNumber,2) as ProductType 
+FROM SalesLT.Product AS p
+
+-- Retrieve only products with a numeric size
+SELECT p.ProductID as ProductID, UPPER(p.Name) AS Name, ROUND(Weight,0) as ApproxWeight, YEAR(p.SellStartDate) as SellStartYear , 
+DATENAME(mm,p.SellStartDate) as SellStartMonth, LEFT(p.ProductNumber,2) as ProductType
+FROM SalesLT.Product AS p 
+WHERE  ISNUMERIC(p.Size) = 1
