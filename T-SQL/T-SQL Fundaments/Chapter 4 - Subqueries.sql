55ee3797801d03603fd7de8cd015c6c6b17457c5
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2020-01-09 21:58:31 +0100

    Update Chapter 4 - Subqueries.sql

--- T-SQL/T-SQL Fundaments/Chapter 4 - Subqueries.sql
+++ T-SQL/T-SQL Fundaments/Chapter 4 - Subqueries.sql
@@ -22,20 +22,30 @@ ORDER BY COUNT(*) DESC
 -- Write a query that returns employees who did not place orders on or after May 1, 2016:
 -- Tables involved: HR.Employees and Sales.Orders
 
+SELECT e.empid, e.firstname, e.lastname
+FROM HR.Employees as e
+WHERE e.empid NOT IN (SELECT DISTINCT empid
+FROM Sales.Orders WHERE orderdate >= '20160501')
+
 -- Write a query that returns countries where there are customers but not employees:
 -- Tables involved: Sales.Customers and HR.Employees
+SELECT DISTINCT c.country
+FROM Sales.Customers AS c
+WHERE c.country NOT IN (SELECT DISTINCT country
+FROM HR.Employees as e)
+
 
 -- Write a query that returns for each customer all orders placed on the customer’s last day of activity:
 -- Table involved: Sales.Orders
 
--- Write a query that returns countries where there are customers but not employees:
--- Tables involved: Sales.Customers and HR.Employees
+SELECT custid, orderid, orderdate, empid
+FROM Sales.Orders AS o
+WHERE orderdate = 
+(SELECT MAX(o1.orderdate)
+FROM Sales.Orders AS o1
+WHERE o1.custid = o.custid)
+ORDER BY custid
 
--- Write a query that returns countries where there are customers but not employees:
--- Tables involved: Sales.Customers and HR.Employees
-
--- Write a query that returns for each customer all orders placed on the customer’s last day of activity:
--- Table involved: Sales.Orders
 
 -- Write a query that returns customers who placed orders in 2015 but not in 2016:
 -- Tables involved: Sales.Customers and Sales.Orders
