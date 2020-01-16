e95fb2249ec927fe1f0fcbfe8184912dc009b067
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2020-01-16 23:05:35 +0100

    Update Chapter 6 - Set operators.sql

--- T-SQL/T-SQL Fundaments/Chapter 6 - Set operators.sql
+++ T-SQL/T-SQL Fundaments/Chapter 6 - Set operators.sql
@@ -2,14 +2,60 @@
 
 -- Write a query that generates a virtual auxiliary table of 10 numbers in the range 1 through 10 without using a looping construct. You do not need to guarantee any order of the rows in the output of your solution:
 
+SELECT DISTINCT o1.custid, o1.empid
+FROM Sales.Orders as o1
+WHERE MONTH(o1.orderdate) = '01' AND YEAR(o1.orderdate) = 2016
+EXCEPT
+SELECT DISTINCT o1.custid, o1.empid
+FROM Sales.Orders as o1
+WHERE MONTH(o1.orderdate) = '02' AND YEAR(o1.orderdate) = 2016
+
 -- Write a query that returns customer and employee pairs that had order activity in January 2016 but not in February 2016:
 -- Table involved: Sales.Orders table
+
+SELECT DISTINCT o1.custid, o1.empid
+FROM Sales.Orders as o1
+WHERE MONTH(o1.orderdate) = '01' AND YEAR(o1.orderdate) = 2016
+INTERSECT
+SELECT DISTINCT o1.custid, o1.empid
+FROM Sales.Orders as o1
+WHERE MONTH(o1.orderdate) = '02' AND YEAR(o1.orderdate) = 2016
 
 -- Write a query that returns customer and employee pairs that had order activity in both January 2016 and February 2016:
 -- Table involved: Sales.Orders
 
+SELECT o1.custid, o1.empid
+FROM Sales.Orders as o1
+WHERE MONTH(o1.orderdate) = '01' AND YEAR(o1.orderdate) = 2016
+INTERSECT
+
+SELECT o1.custid, o1.empid
+FROM Sales.Orders as o1
+WHERE MONTH(o1.orderdate) = '02' AND YEAR(o1.orderdate) = 2016
+
+EXCEPT
+SELECT o1.custid, o1.empid
+FROM Sales.Orders as o1
+WHERE YEAR(o1.orderdate) = 2015
+
 -- Write a query that returns customer and employee pairs that had order activity in both January 2016 and February 2016 but not in 2015:
 --Table involved: Sales.Orders
+
+
+SELECT o1.custid, o1.empid
+FROM Sales.Orders as o1
+WHERE MONTH(o1.orderdate) = '01' AND YEAR(o1.orderdate) = 2016
+INTERSECT
+
+SELECT o1.custid, o1.empid
+FROM Sales.Orders as o1
+WHERE MONTH(o1.orderdate) = '02' AND YEAR(o1.orderdate) = 2016
+
+EXCEPT
+SELECT o1.custid, o1.empid
+FROM Sales.Orders as o1
+WHERE YEAR(o1.orderdate) = 2015
+
 
 --You are given the following query:
 SELECT country, region, city
