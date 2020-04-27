27e3f81937f0ede569f4c5f4d4679a187037fa2d
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2020-04-27 20:00:18 +0200

    Update noReplacementSimulation.py

--- Python/6.00.2x - Computational Thinking and Data Science/Unit 3/Lecture 7/noReplacementSimulation.py
+++ Python/6.00.2x - Computational Thinking and Data Science/Unit 3/Lecture 7/noReplacementSimulation.py
@@ -4,6 +4,7 @@ Created on Sun Apr 26 21:57:16 2020
 
 @author: piotr
 """
+import random
 
 def noReplacementSimulation(numTrials):
     '''
@@ -13,7 +14,28 @@ def noReplacementSimulation(numTrials):
     drawn. Returns the a decimal - the fraction of times 3 
     balls of the same color were drawn.
     '''
-    # Your code here
+    estimates = []
+    for t in range(numTrials):
+        ballGuess = drawnBalls()
+        estimates.append(ballGuess)
+        
+    results = sum(estimates)/len(estimates)
+    return results
+
+def drawnBalls():
+    bucked = ["red","red","red","green","green","green"]
+    
+    for i in range(3):
+        bucked.remove(bucked[random.randint(0,len(bucked)-1)])
     
     
+    red_count = bucked.count("red")
+    green_count = bucked.count("green")
     
+    if green_count == 3:
+        return 1
+    elif red_count == 3:
+        return 1
+    else: 
+        return 0
+
